aba10a123945cdb0705b1418fb10ec273411c327
Rachel Freedman <rachel.alex.freedman93@gmail.com>
2021-10-15 17:38:35 -0700

    add beta=10

--- scripts/quadruped_walk/2000/beta_experiments/run_PEBBLE.sh
+++ scripts/quadruped_walk/2000/beta_experiments/run_PEBBLE.sh
@@ -1,5 +1,5 @@
 for seed in 12345 23451 34512 45123 51234; do
-  for beta in 0.01 0.1 1.0 100; do
+  for beta in 0.01 0.1 1 10 100; do
       python train_PEBBLE.py  env=quadruped_walk seed=$seed agent.params.actor_lr=0.0001 agent.params.critic_lr=0.0001 gradient_update=1 activation=tanh num_unsup_steps=9000 num_train_steps=1000000 num_interact=30000 max_feedback=2000 reward_batch=200 reward_update=50 feed_type=1 teacher_beta=$beta teacher_gamma=1 teacher_eps_mistake=0 teacher_eps_skip=0 teacher_eps_equal=0.1
   done
 done
